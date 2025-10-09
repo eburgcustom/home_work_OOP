1ef6f8924c40f85f1fe785ca3a8953c73bf92c3f
Ildar <gif8686@gmail.com>
2025-10-09 20:39:37 +0500

    category.py&product.py updated

--- src/category.py
+++ src/category.py
@@ -9,6 +9,22 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product) -> None:
+        """Добавляет объект Product в категорию."""
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product")
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Геттер: возвращает строку со списком товаров."""
+        result = ""
+        for p in self.__products:
+            result += f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт.\n"
+        return result.strip()
