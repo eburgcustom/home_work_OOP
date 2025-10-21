1616decbe86c05fc74c0b39cf2adef7cb8a1426a
Ildar <gif8686@gmail.com>
2025-10-21 23:15:06 +0500

    class Smartphone&LawnGlass done

---
+++ src/lawn_grass.py
@@ -0,0 +1,24 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 country: str, germination_period: str, color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __add__(self, other: Product) -> float:
+        """Складывает газонную траву по общей стоимости (цена * количество).
+
+        Возвращает:
+            float: Суммарная стоимость всех трав
+
+        Исключения:
+            TypeError: Если other не является экземпляром класса LawnGrass
+        """
+        if type(other) is LawnGrass:
+            return (self.price * self.quantity) + (other.price * other.quantity)
+        else:
+            raise TypeError("Можно складывать только объекты класса LawnGrass")
