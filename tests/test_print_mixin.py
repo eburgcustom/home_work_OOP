65ec630141174f50167156e0c2e004fd380a3162
Ildar <gif8686@gmail.com>
2025-10-23 20:10:21 +0500

    new tests for class BaseProduct&PrintMixin

---
+++ tests/test_print_mixin.py
@@ -0,0 +1,26 @@
+from src.product import Product
+from src.smartphone import Smartphone
+from src.lawn_grass import LawnGrass
+
+
+def test_print_mixin(capsys):
+    Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    message = capsys.readouterr()
+    assert message.out.strip() == "Создан объект класса Product('Iphone 15', '512GB, Gray space', 210000.0, 8)"
+
+    Smartphone("Iphone 15", "512GB, Gray space",
+               210000.0, 8, 98.2, "15", 512, "Gray space")
+    message = capsys.readouterr()
+    assert message.out.strip() == "Создан объект класса Smartphone('Iphone 15', '512GB, Gray space', 210000.0, 8)"
+
+    LawnGrass("Газонная трава 2",
+              "Выносливая трава",
+              450.0,
+              15,
+              "США",
+              "5 дней",
+              "Темно-зеленый")
+    message = capsys.readouterr()
+    assert message.out.strip() == "Создан объект класса LawnGrass('Газонная трава 2', 'Выносливая трава', 450.0, 15)"
+
+
--- tests/test_product.py
+++ tests/test_product.py
@@ -5,6 +5,8 @@ import pytest
 
 from src.product import Product
 
+from src.base_product import BaseProduct
+
 
 def test_product_init(product):
     """Тестирование инициализации продукта"""
@@ -27,6 +29,9 @@ def test_price_setter_valid(product):
 
 def test_price_setter_invalid(capsys, product):
     """Тестирование сеттера цены с некорректным значением"""
+    # Очищаем буфер после создания объекта (который происходит в фикстуре)
+    capsys.readouterr()
+
     product.price = -100.0
     message = capsys.readouterr()
     assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
@@ -70,3 +75,9 @@ def test_add_products_type_error():
 
     with pytest.raises(TypeError, match="Можно складывать только объекты класса Product"):
         _ = p + 123
+
+
+def test_baseproduct_is_abstract():
+    """Проверяет, что BaseProduct нельзя инстанцировать напрямую."""
+    with pytest.raises(TypeError):
+        BaseProduct("Тест", "Описание", 100, 1)
